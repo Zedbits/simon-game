fe3ab919b6dd32a60e66a5c259c4398af4b0c282
Zed <113933520+Zedbits@users.noreply.github.com>
2022-10-28 14:01:16 -0400

    Update code.py

--- code.py
+++ code.py
@@ -21,12 +21,9 @@ touch6 = touchio.TouchIn(touch_pad6)
 num_pixels = 10
 np = neopixel.NeoPixel(board.NEOPIXEL, num_pixels, brightness=.1, auto_write=False)
 
-win = True
 game_seq = []
-player_seq = []
 button_stat = False
-count = 0
-player_touch = False
+
 game_stat = False
 
 """
@@ -39,47 +36,45 @@ def blink(color, number1, number2):
     np[number1] = color
     np[number2] = color
 
-def lights(value):
-    if value == touch1:
-        if value.value == True:
-            blink((255, 1, 1), 6, 5)
-            np.show()
-            time.sleep(1)
-            player_seq.append(1)
-            player_touch = True
-            blink((0, 0, 0), 6, 5)
-            np.show()
-        time.sleep(.05)
-    if value == touch2:
-        if value.value == True:
-            blink((1, 255, 1), 9, 8)
-            np.show()
-            time.sleep(1)
-            player_seq.append(0)
-            player_touch = True
-            blink((0, 0, 0), 9, 8)
-            np.show()
-        time.sleep(.05)
-    if value == touch6:
-        if value.value == True:
-            blink((1, 1, 255), 3, 4)
-            np.show()
-            time.sleep(1)
-            player_seq.append(2)
-            player_touch = True
-            blink((0, 0, 0), 3, 4)
-            np.show()
-        time.sleep(.05)
-    if value == touch5:
-        if value.value == True:
-            blink((255, 255, 0), 0, 1)
-            np.show()
-            time.sleep(1)
-            player_seq.append(3)
-            player_touch = True
-            blink((0, 0, 0), 0, 1)
-            np.show()
-        time.sleep(.05)
+def lights():
+    while not touch1.value and not touch2.value and not touch5.value and not touch6.value:
+        pass
+    if touch1.value:
+        blink((255, 1, 1), 6, 5)
+        np.show()
+        time.sleep(1)
+        player_val = 1
+        player_touch = True
+        blink((0, 0, 0), 6, 5)
+        np.show()
+    time.sleep(.05)
+    if touch2.value:
+        blink((1, 255, 1), 9, 8)
+        np.show()
+        time.sleep(1)
+        player_val = 0
+        player_touch = True
+        blink((0, 0, 0), 9, 8)
+        np.show()
+    time.sleep(.05)
+    if touch6.value:
+        blink((1, 1, 255), 3, 4)
+        np.show()
+        time.sleep(1)
+        player_val = 2
+        player_touch = True
+        blink((0, 0, 0), 3, 4)
+        np.show()
+    time.sleep(.05)
+    if touch5.value:
+        blink((255, 255, 0), 0, 1)
+        np.show()
+        time.sleep(1)
+        player_val = 3
+        player_touch = True
+        blink((0, 0, 0), 0, 1)
+        np.show()
+    time.sleep(.05)
 
 def comp():
     rand = random.randint(0, 3)
@@ -115,26 +110,28 @@ def comp():
             time.sleep(1)
 
 def reset():
-    player_seq = []
+    player_val = -1
     game_seq = []
     game_stat == False
 
 def check():
-    if game_seq == player_seq:
-        win = True
-        pass
+    count = 0
+    player_val = -1
+    if game_seq[count] == player_val:
+        count += 1
+        np.fill((0, 255, 0))
+        np.fill((0, 0, 0))
+        print(player_val)
+        print(game_seq[count])
     else:
-        for i in range(5):
-            win = False
-            np.fill((255, 0, 0))
-            reset()
+        np.fill((255, 0, 0))
+        np.fill((0, 0, 0))
+        reset()
+
+
 
 def player():
-    while not player_touch:
-        lights(touch1)
-        lights(touch2)
-        lights(touch5)
-        lights(touch6)
+    lights()
 
 
 while True:
@@ -146,5 +143,5 @@ while True:
     else:
         comp()
         player()
-        print(player_touch)
+        check()
 
